a061517e9b43b462e64166948ad7c3533d615481
Patrick Heneghan <discopatrick@gmail.com>
2020-02-04 00:05:55 +0000

    Ensure db session removed after every request

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
+from database import db_session
 from exceptions import api_errors
 from resources.contact import ContactResource
 
@@ -8,6 +9,12 @@ app = Flask(__name__)
 api = Api(app, errors=api_errors)
 
 api.add_resource(ContactResource, '/contact', '/contact/<contact_id>')
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
 
 if __name__ == '__main__':
     app.run()
